b32978a59b62e35fbfc6839d63076cb61c030c27
Nguyễn Ngọc Thiện <162625134+thienitcoder2004@users.noreply.github.com>
2024-08-28 20:41:46 +0700

    Tìm số dương đầu tiên trong ma trận 2 chiều

---
+++ Array/TimSoDuongDauTienTrongMang2Chieu.java
@@ -0,0 +1,70 @@
+import java.util.Scanner;
+
+public class TimSoDuongDauTienTrongMang2Chieu {
+    public static int MIN = -50;
+    public static int MAX = 50;
+
+    // Nhập số dòng
+    public static int nhapDong(Scanner scan) {
+        int n;
+        do {
+            System.out.print("Vui long nhap so dong: ");
+            n = Integer.parseInt(scan.nextLine());
+        } while (n < 1);
+        return n;
+    }
+
+    // Nhập số cột
+    public static int nhapCot(Scanner scan) {
+        int n;
+        do {
+            System.out.print("Vui long nhap so cot: ");
+            n = Integer.parseInt(scan.nextLine());
+        } while (n < 1);
+        return n;
+    }
+
+    // Nhập ma trận
+    public static int[][] nhapMaTran(int soCot, int soDong, Scanner scan) {
+        int a[][] = new int[soDong][soCot];
+        for (int i = 0; i < soDong; i++) {
+            for (int j = 0; j < soCot; j++) {
+                a[i][j] = MIN + (int) (Math.random() * ((MAX - MIN) + 1));
+            }
+        }
+        return a;
+    }
+
+    // Xuất ma trận
+    public static void xuatMaTran(int a[][], int soCot, int soDong) {
+        for (int i = 0; i < soDong; i++) {
+            for (int j = 0; j < soCot; j++) {
+                System.out.print(a[i][j] + "  "); // in một dòng
+            }
+            System.out.println(""); // xuống dòng
+        }
+    }
+
+    // Hàm tìm số nguyên dương đầu tiên xuất hiện
+    public static void TimSoDuongDauTien(int a[][], int soCot, int soDong) {
+        for (int i = 0; i < soDong; i++) {
+            for (int j = 0; j < soCot; j++) {
+                if (a[i][j] > 0) {
+                    System.out.println("So duong dau tien trong ma tran la: " + a[i][j] + " [" + i + "][" + j + "]");
+                    return;
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int soDong = nhapDong(scan);
+        int soCot = nhapCot(scan);
+
+        int a[][] = nhapMaTran(soCot, soDong, scan);
+        System.out.println("Danh sach mang 2 chieu: ");
+        xuatMaTran(a, soCot, soDong);
+        TimSoDuongDauTien(a, soCot, soDong);
+    }
+}
