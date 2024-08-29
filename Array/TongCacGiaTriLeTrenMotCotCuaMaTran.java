9a92391c92236db252e5b4ea2ac73b3dfda0c7b9
Nguyễn Ngọc Thiện <162625134+thienitcoder2004@users.noreply.github.com>
2024-08-29 13:52:41 +0700

    Tính tổng các giá trị lẻ trên một cột của ma trận

---
+++ Array/TongCacGiaTriLeTrenMotCotCuaMaTran.java
@@ -0,0 +1,70 @@
+import java.util.Scanner;
+
+public class TongCacGiaTriLeTrenMotCotCuaMaTran {
+    public static int MIN = -50;
+    public static int MAX = 50;
+
+    // Nhập số dòng
+    public static int nhapDong(Scanner scan) {
+        int n;
+        do {
+            System.out.print("Vui long nhap so dong: ");
+            n = Integer.parseInt(scan.nextLine());
+        } while (n < 1);
+        return n;
+    }
+
+    // Nhập số cột
+    public static int nhapCot(Scanner scan) {
+        int n;
+        do {
+            System.out.print("Vui long nhap so cot: ");
+            n = Integer.parseInt(scan.nextLine());
+        } while (n < 1);
+        return n;
+    }
+
+    // Nhập ma trận
+    public static int[][] nhapMaTran(int soCot, int soDong, Scanner scan) {
+        int a[][] = new int[soDong][soCot];
+        for (int i = 0; i < soDong; i++) {
+            for (int j = 0; j < soCot; j++) {
+                a[i][j] = MIN + (int) (Math.random() * ((MAX - MIN) + 1));
+            }
+        }
+        return a;
+    }
+
+    // Xuất ma trận
+    public static void xuatMaTran(int a[][], int soCot, int soDong) {
+        for (int i = 0; i < soDong; i++) {
+            for (int j = 0; j < soCot; j++) {
+                System.out.print(a[i][j] + "  "); // in một dòng
+            }
+            System.out.println(""); // xuống dòng
+        }
+    }
+
+    // Hàm tính tổng các giá trị trên một cột của ma trận
+    public static void TongCacGiaTriLeTrenMotCot(int a[][], int soDong, int soCot) {
+        for (int i = 0; i < soDong; i++) {
+            long sum = 0;
+            for(int j=0;j<soCot;j++){
+                if(a[i][j] % 2 != 0)
+                    sum += a[i][j];
+            }
+            System.out.println("Tong cac gia tri cot " + i + " = "+ sum);
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int soDong = nhapDong(scan);
+        int soCot = nhapCot(scan);
+
+        int a[][] = nhapMaTran(soCot, soDong, scan);
+        System.out.println("Danh sach mang 2 chieu: ");
+        xuatMaTran(a, soCot, soDong);
+        TongCacGiaTriLeTrenMotCot(a, soDong, soCot);
+    }
+}
